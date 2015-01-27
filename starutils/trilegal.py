494e0dba767e508427bbde0f1f7f00b9fefa49ee
Timothy Morton <tim.morton@gmail.com>
2015-01-27 11:37:26 -0500

    removed usage of sed in get_trilegal

--- starutils/trilegal.py
+++ starutils/trilegal.py
@@ -23,19 +23,25 @@ def get_trilegal(filename,ra,dec,folder='.',
     except UnitsError:
         c = SkyCoord(ra,dec,unit='deg')
     l,b = (c.galactic.l.value,c.galactic.b.value)
-    outfile = '{}/{}.dat'.format(folder,filename)
+    if not re.search('\.dat$',filename):
+        outfile = '{}/{}.dat'.format(folder,filename)
+    else:
+        outfile = '{}/{}'.format(folder,filename)
     AV = get_AV_infinity(l,b,frame='galactic')
     cmd = 'get_trilegal %s %f %f %f %i %.3f %.2f %s 1 %.1f %s' % (trilegal_version,l,b,
                                                                   area,binaries,AV,sigma_AV,
                                                                   filterset,maglim,outfile)
     sp.Popen(cmd,shell=True).wait()
     if convert_h5:
-        sp.Popen("sed -i 's/#Gc/Gc/' {}".format(outfile),shell=True).wait() #uncomments first line
         df = pd.read_table(outfile,delim_whitespace=True,comment='#')
+        df = df.rename(columns={'#Gc':'Gc'})
         for col in df.columns:
             if col not in NONMAG_COLS:
                 df.rename(columns={col:'{}_mag'.format(col)},inplace=True)
-        h5file = '{}/{}.h5'.format(folder,filename)
+        if not m.search('\.h5$', filename):
+            h5file = '{}/{}.h5'.format(folder,filename)
+        else:
+            h5file = '{}/{}'.format(folder,filename)
         df.to_hdf(h5file,'df')
         store = pd.HDFStore(h5file)
         attrs = store.get_storer('df').attrs
