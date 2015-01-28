9d9f8f049ea0d2c0b15b5c4e8951e2769f68155f
Timothy Morton <tim.morton@gmail.com>
2015-01-28 13:02:32 -0500

    version increment, improvements to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, Extension, find_packages
 from Cython.Build import cythonize
 from Cython.Distutils import build_ext
-import numpy
+import os, import sys
 
 def readme():
     with open('README.rst') as f:
