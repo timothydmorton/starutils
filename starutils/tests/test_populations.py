b6063e23f0b52359863d841e4ea72e904633ef81
Timothy Morton <tim.morton@gmail.com>
2015-01-30 16:05:31 -0500

    add tests to make sure correct properties are restored

--- starutils/tests/test_populations.py
+++ starutils/tests/test_populations.py
@@ -10,23 +10,26 @@ import tempfile
 TMP = tempfile.gettempdir()
 
 def test_raghavan(filename=os.path.join(TMP,'test_raghavan.h5')):
-    pop = Raghavan_BinaryPopulation(1, n=100)
+    pop = Raghavan_BinaryPopulation(1, n=100, qmin=0.2)
     pop.save_hdf(filename, overwrite=True)
     pop2 = Raghavan_BinaryPopulation().load_hdf(filename)
 
     #test to make sure correct properties are there
     [pop2.q_fn, pop2.qmin, pop2.P_fn, pop2.ecc_fn, pop2.minmass]
 
+    assert pop2.qmin == 0.2
     os.remove(filename)
 
 def test_multiple(filename=os.path.join(TMP,'test_multiple.h5')):
-    pop = MultipleStarPopulation(1, n=100)
+    pop = MultipleStarPopulation(1, n=100, minmass=0.15)
     pop.save_hdf(filename, overwrite=True)
     pop2 = MultipleStarPopulation().load_hdf(filename)
 
     #test to make sure correct properties are there
     [pop2.f_binary, pop2.f_triple, pop2.qmin, pop2.minmass,
      pop2.period_long_fn, pop2.period_short_fn, pop2.ecc_fn]
+
+    assert pop2.minmass == 0.15
 
     os.remove(filename)
 
@@ -40,6 +43,8 @@ def test_colormatch(filename=os.path.join(TMP,'test_colormatch.h5')):
 
     #test to make sure correct properties are there
     [pop2.mags, pop2.colors, pop2.colortol, pop2.starfield]
+
+    assert pop2.mags==mags
 
     os.remove(filename)
 
